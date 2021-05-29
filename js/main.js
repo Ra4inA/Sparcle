1a324f47808a9efc7b85092ac245a9b8ea4a62f1
Ra4inA <vitya-sorokin-92@mail.ru>
2021-05-29 11:32:54 +0300

    Form

--- js/main.js
+++ js/main.js
@@ -57,3 +57,37 @@ $(window).resize(function(){
 
 $(".menu__btn").on("click", showMenu);
 
+// ===============================================================
+let $type = $("#typeOfCleaning");
+let $typeTime;
+let $typePrice;
+function readValue($input){
+	value = $input.val();
+	return value;
+}
+
+$("input, .number__button").on("change click", function(){
+	if($("#commercial").is(":checked")){
+		$type.text("Commercial");
+		$typeTime = 2;
+		$typePrice = 50;
+	} else {
+		$type.text("Residential");
+		$typeTime = 1.5;
+		$typePrice = 35;
+	}
+
+
+
+	$("#bedrooms").text(readValue($("#count-bedrooms")));
+	$("#bathrooms").text(readValue($("#count-bathrooms")));
+
+	$("#timeOfCleaning").text(function(){
+		if($("#bedrooms").text() == "0" & $("#bathrooms").text() == "0"){return 0};
+		return ($typeTime + readValue($("#count-bedrooms")) * 1.5 + readValue($("#count-bathrooms")) * 1.5);
+	})
+	$("#price").text(function(){
+		if($("#bedrooms").text() == "0" & $("#bathrooms").text() == "0"){return 0};
+		return ($typePrice + readValue($("#count-bedrooms")) * 15.44 + readValue($("#count-bathrooms")) * 10.47).toFixed(2);
+	});
+});
